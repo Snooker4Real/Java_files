b7673caa7d950121cc1d296a1a484528c6f7b8ab
Snooker4Real <65620947+Snooker4Real@users.noreply.github.com>
2020-12-19 13:27:04 +0100

    Add files via upload

---
+++ Access modifier/src/package2/C.java
@@ -0,0 +1,9 @@
+package package2;
+import package2.*;
+
+public class C {
+
+    public String publicMessage = "This is public";
+    protected String protectedMessage = "This is protected";
+            String defaultMessage = "This is the default";
+}
