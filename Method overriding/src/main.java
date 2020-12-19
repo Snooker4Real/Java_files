9f66d53f8e358cd7d9bf4185ae0f4c7c537dc3a7
Snooker4Real <65620947+Snooker4Real@users.noreply.github.com>
2020-12-19 12:10:09 +0100

    Add files via upload

---
+++ Method overriding/src/main.java
@@ -0,0 +1,17 @@
+public class main {
+
+    public static void main(String[] args) {
+        // method overriding =  Declaring a method in a sub class,
+        //                      which is already present in parent class.
+        //                      done so that a child class can give its own implementation
+
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+
+        animal.speak();
+
+
+
+
+    }
+}
