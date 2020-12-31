e76c5d45bc53054070a4180bb6ecce5d4f3bd664
Snooker4Real <65620947+Snooker4Real@users.noreply.github.com>
2020-12-31 16:26:50 +0100

    Add files via upload

--- BorderLayout/src/Main.java
+++ BorderLayout/src/Main.java
@@ -29,6 +29,8 @@ public class Main {
         panel4.setBackground(Color.magenta);
         panel5.setBackground(Color.blue);
 
+        panel5.setLayout(new BorderLayout());
+
         panel1.setPreferredSize(new Dimension(100,100));
         panel2.setPreferredSize(new Dimension(100,100));
         panel3.setPreferredSize(new Dimension(100,100));
@@ -36,6 +38,36 @@ public class Main {
         panel5.setPreferredSize(new Dimension(100,100));
 
         //-------------- sub panels----------------
+
+        JPanel panel6 = new JPanel();
+        JPanel panel7 = new JPanel();
+        JPanel panel8 = new JPanel();
+        JPanel panel9 = new JPanel();
+        JPanel panel10 = new JPanel();
+
+        panel6.setBackground(Color.black);
+        panel7.setBackground(Color.darkGray);
+        panel8.setBackground(Color.gray);
+        panel9.setBackground(Color.lightGray);
+        panel10 .setBackground(Color.white);
+
+        panel5.setLayout(new BorderLayout());
+
+        panel6.setPreferredSize(new Dimension(50,50));
+        panel7.setPreferredSize(new Dimension(50,50));
+        panel8.setPreferredSize(new Dimension(50,50));
+        panel9.setPreferredSize(new Dimension(50,50));
+        panel10.setPreferredSize(new Dimension(50,50));
+
+        panel5.add(panel6, BorderLayout.NORTH);
+        panel5.add(panel7, BorderLayout.SOUTH);
+        panel5.add(panel8, BorderLayout.WEST);
+        panel5.add(panel9, BorderLayout.EAST);
+        panel5.add(panel10, BorderLayout.CENTER);
+
+
+
+
 
         frame.add(panel1, BorderLayout.NORTH);
         frame.add(panel2, BorderLayout.WEST);
