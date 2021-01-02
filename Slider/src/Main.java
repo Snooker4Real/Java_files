bf931c6d183b73732194f2fcbd611d263192b196
Snooker4Real <65620947+Snooker4Real@users.noreply.github.com>
2021-01-02 17:11:23 +0100

    Add files via upload

---
+++ Slider/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // JSlider = A GUI component that lets us enter a value by using an adjustable slidng knob on a track
+
+        SliderDemo sliderDemo = new SliderDemo();
+
+    }
+}
