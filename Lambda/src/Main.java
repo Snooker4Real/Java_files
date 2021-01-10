86d07dd01de23f487bb319bed87e1d6e0f6bf99a
Snooker4Real <65620947+Snooker4Real@users.noreply.github.com>
2021-01-10 16:35:21 +0100

    Add files via upload

---
+++ Lambda/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Lambda expressions = feature for Java 8 and above
+        //                      also known as an anonymous method
+        //                      a shorter way to write anonymous classes with only one method
+
+        //                      need to use a functional interface or use a pre-defined functional interface
+        //                      they contain only one abstract method
+        //                      ex. ActionListener, Runnable, (user-defined)
+
+        //                      A lambda expression can be used in any place where a functional interface is required
+        //                      How to use lambda expressions:
+        //                      (arguments) -> {statement/s}
+
+        /*
+        String name = "Jonathan";
+        char symbol = '!';
+        MyInterface myInterface = (x,y) -> {
+            System.out.println("Hello World!");
+            System.out.println("It is a nice day "+x+y);
+
+        };
+        MyInterface myInterface2 = (x,y) -> {
+            System.out.println("Hello "+x+y);
+        };
+        myInterface2.message(name, symbol);
+
+         */
+        MyFrame myFrame = new MyFrame();
+    }
+}
